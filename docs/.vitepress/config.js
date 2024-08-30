f807d5be0d488baebe7f962ccb171c38dda037b4
tenado <690784374@qq.com>
2024-08-30 15:24:50 +0800

    chore: 基础代码

---
+++ docs/.vitepress/config.js
@@ -0,0 +1,14 @@
+module.exports = {
+  base: "/tenado-icon/",
+  title: "字体图标库",
+  description: "欢迎来到我的自定义字体图标库。",
+  vite: {
+    build: {
+      rollupOptions: {
+        output: {
+          format: "es",
+        },
+      },
+    },
+  },
+};
