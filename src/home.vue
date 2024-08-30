0882b65654174b9a90e9e97b30ee85a3a409eec0
tenado <690784374@qq.com>
2024-08-30 16:04:18 +0800

    1

---
+++ src/home.vue
@@ -0,0 +1,26 @@
+<template>
+  <div class="home-wrap">
+    <Icons :query="query"></Icons>
+  </div>
+</template>
+
+<script>
+import Icons from "./icons.vue";
+export default {
+  components: {
+    Icons,
+  },
+  data() {
+    return {
+      query: "",
+    };
+  },
+};
+</script>
+
+<style scoped lang="scss">
+.home-wrap {
+  max-width: 1320px;
+  margin: 0 auto;
+}
+</style>
